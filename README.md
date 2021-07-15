1053ee39eb26808c72c1817ab21f249a05607fce
Jan Biernacik <jan.biernacik@e.email>
2021-07-15 13:57:29 +0200

    Initial Stable release
    - Ability to disable the auto-reset on day change feature
    - Minor visual fixes
    - Better Windows and other non-*NIX OS compatibility
    - Functions no longer get loop-defined
    - More clear variable names
    - Further minor optimization
    - Easter eggs :)!

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # CLINOTE
-![](demos/demo1.jpg)
+![](demos/demo1.png)
 The new CLINOTE written in Python. Now even better featuring features like:
 - Resetting the file via a command
 - Deleting the last note
-- Auto-resetting all tasks when the day changes (option to disable coming soon!)
+- Auto-resetting all tasks when the day changes (can be enabled/disabled with `:dc`)
 - Better formating
+CLINOTE is a perfect TO-DO and note-taking program for people who depend much on the terminal like users of a standalone tiling window manager or just people with a low-end computer.
 # Is the C version dead?
 No, it is on hold. I am currently taking my time to learn Python before I dive into C. It will eventually get picked up by me or a contributor again.
 # Commands
@@ -13,8 +14,14 @@ No, it is on hold. I am currently taking my time to learn Python before I dive i
 :r(eset) - Reset all notes
 :d(elete) - Delete last note
 :h(elp) / :? - Get help with commands
+:d(ay)c(hange) - Disable/Enable task reset on day change
 ```
+# Windows support
+This program is written with *NIX support in mind but I try to make it as compatible as possible with Windows and other non-UNIX operating systems. If you encounter any issues/errors please make sure to open a GitHub issue so I can fix it.
 # Installation
+Requirements:
+- Python3
+
 ```
 git clone https://github.com/JBNCK/CLINOTE
 cd CLINOTE/
--- src/notes.py
+++ src/notes.py
@@ -2,7 +2,7 @@
 import os
 import getpass
 from datetime import date
-i = 1
+loop = 1
 
 user = str(getpass.getuser())
 today_date = str(date.today())
@@ -13,21 +13,44 @@ try:
 except:
     os.chdir("C:\\Users\\" + user)
 
+def read_notes():
+        notes = open(".NOTEFILE", "r")
+        print("---------------------------- TO-DO " + today_date + " ----------------------------")
+        print(notes.read() + "\n")
+        notes.close()
+
 def reset_notes():
     notes = open(".NOTEFILE", "w")
     notes.write("")
     notes.close()
 
-try:
-    date_file = open(".datefile", "r")
+def reset_on_day_change_function():
+    date_file = open(".DATEFILE", "r")
     today_date_from_datefile = str(date_file.read())
     if today_date_from_datefile != today_date:
         reset_notes()
+    else:
+        print("Not resetting.")
     date_file.close()
+
+try:
+    reset_cycle = open(".DATERESETFILE", "r")
+    reset_on_day_change = str(reset_cycle.read())
+    print(reset_on_day_change)
+    reset_cycle.close()
+    if reset_on_day_change == "reset_on_day_change=true":
+        reset_on_day_change_function()
+    else:
+        print("Not true.")
 except:
-    date_file = open(".datefile", "w")
-    date_file.write(today_date)
-    date_file.close()
+    reset_cycle = open(".DATERESETFILE", "w")
+    reset_on_day_change = str("reset_on_day_change=true")
+    reset_cycle.write(reset_on_day_change)
+    reset_cycle.close()
+
+date_file = open(".DATEFILE", "w")
+date_file.write(today_date)
+date_file.close()
 
 # Copied from some random website idk how this works but it does:
 def remove_last_line():
@@ -46,6 +69,12 @@ def append_note(note):
     notes.write("\n- " + note)
     notes.close()
 
+def clear_screen():
+    try:
+        os.system("clear")
+    except:
+        os.system("cls")
+
 def get_help():
     clear_screen()
     print("Commands: ")
@@ -53,24 +82,14 @@ def get_help():
     print(":r(eset) - Reset all notes")
     print(":d(elete) - Delete last note")
     print(":h(elp) / :? - Get help with commands")
+    print(":d(ay)c(hange) - Disable/Enable task reset on day change")
 
     print("\nTo write to-dos just write out the note and press ENTER")
     input("Press ENTER to continue...")
 
-while i == 1:
-    def clear_screen():
-        try:
-            os.system("clear")
-        except:
-            os.system("cls")
-
+while loop == 1:
     clear_screen()
     try:
-        def read_notes():
-            notes = open(".NOTEFILE", "r")
-            print("-------------------------------- TO-DO " + today_date + " --------------------------------")
-            print(notes.read() + "\n")
-            notes.close()
         read_notes()
     except FileNotFoundError:
         notes = open(".NOTEFILE", "w")
@@ -87,5 +106,31 @@ while i == 1:
         reset_notes()
     elif note == ":delete" or note == ":d":
         remove_last_line()
+    elif note == ":c" or note == ":daychange" or note == ":dc":
+        reset_cycle = open(".DATERESETFILE", "r")
+        reset_on_day_change = str(reset_cycle.read())
+        if reset_on_day_change == "reset_on_day_change=true":
+            reset_cycle.close()
+            reset_cycle = open(".DATERESETFILE", "w")
+            reset_cycle.write("reset_on_day_change=false")
+            reset_cycle.close()
+            clear_screen()
+            print("Tasks will no longer be reset on day change.")
+            input("Press ENTER to continue...")
+        else:
+            reset_cycle.close()
+            reset_cycle = open(".DATERESETFILE", "w")
+            reset_cycle.write("reset_on_day_change=true")
+            clear_screen()
+            reset_cycle.close()
+            print("All tasks will be reset on day change.")
+            input("Press ENTER to continue...")
+    elif note == ":jan" or note == ":janbiernacik":
+        clear_screen()
+        print("011000100110100101100101011100100111 :)")
+        input("Press ENTER to continue...")
+    elif note == ":amogus":
+        print("sus")
+        input("Pressus ENTERUS para continuus...")
     else:
         append_note(str(note))
